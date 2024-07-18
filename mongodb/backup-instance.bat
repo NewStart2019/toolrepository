db2a66ca5d9a378d2136ed211629a0297b9c8cc9
zhanqh <2855942332@qq.com>
2024-07-18 17:27:03 +0800

    mongodb备份脚本

---
+++ mongodb/backup-instance.bat
@@ -0,0 +1,28 @@
+@rem 备份单个节点的数据库所有数据为 bson文件
+
+@echo off
+chcp 65001 > nul
+
+call :get_time datetime
+set base_path=G:\mongodb\backupData
+mongodump -h 172.16.0.197 --port 27017 --authenticationDatabase admin -u admin -p "123456" -d jtjc --out "%base_path%\%datetime%"
+
+@rem 导入是目录需要指定到 数据库目录下面的bson文件
+@rem mongorestore -h 172.16.0.175 --port 27017 --authenticationDatabase admin -u admin -p "123456" -d jtjc G:\mongodb\backupData\20240718140704\jtjc
+
+:get_time
+setlocal enabledelayedexpansion
+@rem  获取当前日期和时间
+for /f "tokens=1-9 delims=/:. " %%a in ('wmic Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table ^| findstr /r "."') do (
+    set day=00%%a
+    set hour=00%%b
+    set minute=00%%c
+    set month=00%%d
+    set second=00%%e
+    set year=%%f
+)
+
+@rem  格式化日期和时间为所需格式
+set datetime=%year%%month:~-2%%day:~-2%%hour:~-2%%minute:~-2%%second:~-2%
+endlocal  & ( set "%1=%datetime%" )
+exit /b
