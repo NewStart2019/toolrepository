4013fa71b74416edb923de9d1eee2cdf3f5475a2
zhanqh <2855942332@qq.com>
2025-11-19 10:19:01 +0800

    添加ocr基础镜像和node-red部署脚本

--- docker/BatchDeletNoneImages.bat
+++ docker/BatchDeletNoneImages.bat
@@ -2,4 +2,4 @@ rem powershell 执行删除 标签为 none 的镜像
 docker images --filter "dangling=true" --format "{{.ID}}" | ForEach-Object { docker rmi $_ }
 
 rem linux 删除 标签为 none 的镜像
-docker images | grep "<none>" | awk '{print $3}' | xargs -r docker rmi
+docker images --filter "dangling=true" -q | xargs -r docker rmi
