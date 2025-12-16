5bb9c9c670faf62f07c7eaed5a34a754faf8fe95
zhanqh <2855942332@qq.com>
2025-12-16 11:37:15 +0800

    oceanbase部署脚本优化

--- Script_Linux/test.sh
+++ Script_Linux/test.sh
@@ -1,2 +1,9 @@
 # 解压缩gz 包 会删除原来的压缩包文件
 gunzip 文件名称
+
+ROOT_PATH="/root"
+# shellcheck disable=SC2164
+cd $ROOT_PATH;
+mkdir -p ./dist_new;
+tar -xzf $ARCHIVE -C ./dist_new --strip-components=1 && rm -rf ./dist && mv ./dist_new ./dist;
+docker exec nginx-proxy nginx -s reload
